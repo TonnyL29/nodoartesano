2e276a2aab83a0513ea23087d0d7047ed56786e6
Jose <82624339+TonnyL29@users.noreply.github.com>
2023-08-21 22:09:22 -0300

    boostrap + NavBar
    Agrege a modo ejemplo el componente del navbar. Ademas agregre boostrap para que sea mas facil agregar los estilos . Asi usaremos los CSS para cambiar algun estilo en particular.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import NavBar from "./components/navbar/navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={"/"} />
+        <Route path="/category/:categoryId" element={"/"} />
+        <Route path="products/:productId" element={"/"} />
+        <Route path="/category/:category/products/:productId" element={"/"} />
+        <Route path="*/products/:productId" element={"/"} />
+        <Route path="/cart" element={"/"} />
+        <Route path="/nosotros" element={"/"} />
+        <Route path="*" element={"/"} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
